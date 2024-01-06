a79dd763a91c0bc4dc427143d08a9b30b4d28496
heee1 <p1xell@kakao.com>
2024-01-06 18:57:03 +0900

    filter문제 해결함

--- week2-todolist/src/redux/modules/todoList.js
+++ week2-todolist/src/redux/modules/todoList.js
@@ -29,37 +29,40 @@ export const addTodo = (todo) => {
 	};
 
 // 초기값
-const initialState = [
-	{
-		id: 1, // id는 모두 고유값이어야 합니다.
-		title: "리액트 강의보기",
-		body: "챕터 1부터 챕터 12까지 학습",
-		isDone: false
-	},
-	{
-		id: 2, // id는 모두 고유값이어야 합니다.
-		title: "점심 먹기",
-		body: "점심 뭐먹지..?",
-		isDone: false
-	}
-]
+const initialState = {
+	todos: [
+		{
+			id: 1, // id는 모두 고유값이어야 합니다.
+			title: "리액트 강의보기",
+			body: "챕터 1부터 챕터 12까지 학습",
+			isDone: false
+		},
+		{
+			id: 2, // id는 모두 고유값이어야 합니다.
+			title: "점심 먹기",
+			body: "점심 뭐먹지..?",
+			isDone: false
+		}
+	],
+	todoItem: {}
+}
 
 const todoList = (state = initialState, action) => {
     switch(action.type) {
 		// 제목, 내용 입력 후 > 추가하기 버튼 클릭시 작동
 		case ADD_TODO:
 			return{
-				todo: [...state.todos, action.payload],
+				todos: [...state.todos, action.payload],
 			};
 		// todo item의 삭제하기 버튼 클릭시 작동
 		case DELETE_TODO:
 			return{
-				todo: state.todos.filter((todo) => todo.id !== action.payload),
+				todos: state.todos.filter((todo) => todo.id !== action.payload),
 			};
 		// isDone상태 전환 필요시 작동 ( 취소하기 <-> 완료하기 )
 		case TOGGLE_STATUS_TODO:
 			return{
-				todo: state.todos.map((todo) => {
+				todos: state.todos.map((todo) => {
 					if (todo.id !== action.payload) {
 						return todo;
 					} else {
