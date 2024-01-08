5262392d60ac2b093a2c8158818c569c372e2c14
heee1 <p1xell@kakao.com>
2024-01-09 03:05:57 +0900

    미구현: input리렌더링, select선택시변경

---
+++ week3-assignment/src/Modal/MyDropDownA.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+function MyDropDownA({ onClose }){
+    // 박스 바깥으로 나가야함
+    return (
+        <div className="MyDropDownA">
+            <Mask onClick={onClose}></Mask>
+            <DropdownBody>
+                <DropdownContent 
+                    onClick={()=>alert("리액트")}>
+                            리액트 
+                </DropdownContent>
+                <DropdownContent 
+                    onClick={()=>alert("자바")}>
+                            자바 
+                </DropdownContent>
+                <DropdownContent 
+                    onClick={()=>alert("스프링")}>
+                            스프링 
+                </DropdownContent>
+                <DropdownContent
+                    onClick={()=>alert("리액트네이티브")}>
+                            리액트네이티브 
+                </DropdownContent>
+            </DropdownBody>
+        </div>
+    );
+};
+
+export default React.memo(MyDropDownA);
+
+const Mask = styled.div`
+    width: 100%;
+    height: 100vh;
+    inset: 0px;
+    position: fixed;
+`
+const DropdownBody = styled.div`
+    left: 155px;
+    top: 79%;
+    border: 1px solid lightgray;
+    transform: translate(-50%, -50%);
+    border-radius: 12px;
+    box-sizing: border-box;
+    background-color: rgb(255, 255, 255);
+    width: 300px;
+    height: 150px;
+    position: absolute;
+`
+const DropdownContent = styled.div`
+    width:290px;
+    height: 40px;
+    padding-left: 10px;
+    font-size: 14px;
+    &:hover {
+        background: #EEE;
+    }
+    
+`
--- week3-assignment/src/components/SelectArea.jsx
+++ week3-assignment/src/components/SelectArea.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import MyDropdownA from '../Modal/MyDropDownA';
+import MyDropdownB from '../Modal/MyDropDownB';
+import ModalPortal from '../Modal/ModalPortal';
 
 function SelectArea() {
+    // 드랍다운 켜져있는지에 대한 여부
+    const [dropDownAIsOn, setdropDownAIsOn] = useState(false);
+    const [dropDownBIsOn, setdropDownBIsOn] = useState(false);
+    // 드랍다운 상태 토글 함수
+    const dropDownAStatusChanger = () => { 
+        // console.log("DROPDOWN A 실행됨");
+        return setdropDownAIsOn(!dropDownAIsOn) 
+    };
+    const dropDownBStatusChanger = () => { 
+        // console.log("DROPDOWN B 실행됨");
+        return setdropDownBIsOn(!dropDownBIsOn) 
+    };
+    const [currentItem, setCurentItem] = useState("리액트");
+    const [newItem, setNewItem] = useState("");
+
     return (
         <div id="one">
             <Cotainer>
                 <h1>Select</h1>
                 <SelectGroup>
                     <SelectItem>
-                        <Button>
-                            <div>리액트</div>
+                        <Button 
+                            onClick={() => dropDownAStatusChanger()}
+                            // onClick={() => alert(dropDownAIsOn)}
+                            >
+                            <div>{currentItem}</div>
                             <div>▼</div>
                         </Button>
+                        <ModalPortal>
+                            {dropDownAIsOn && <MyDropdownA onClose={dropDownAStatusChanger} />}
+                        </ModalPortal>
                     </SelectItem>
                     <SelectItem>
-                        <Button>
+                        <Button 
+                                onClick={() => dropDownBStatusChanger()}
+                                // onClick={() => alert(dropDownBIsOn)}
+                                >
                             <div>리액트</div>
                             <div>▼</div>
                         </Button>
+                        <ModalPortal>
+                            {dropDownBIsOn && <MyDropdownB onClose={dropDownBStatusChanger} />}
+                        </ModalPortal>
                     </SelectItem>
                 </SelectGroup>
             </Cotainer>
@@ -31,7 +61,6 @@ export default SelectArea
 const Cotainer = styled.div`
     border: 3px solid rgb(221, 221, 221);
     height: 200px;
-    overflow: hidden;
     position: relative;
     margin-top: 50px;
 `
